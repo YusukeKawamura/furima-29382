68c50873ae5eae1907d505a398d823b01a2a14b9
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-17 16:05:42 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_day
+  end
 end
