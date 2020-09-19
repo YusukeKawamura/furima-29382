95eb5a01fdf901e0cb62e7435b3cef2ba822811b
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-20 00:47:50 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :ship_date
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :name, length: { maximum: 40 }
