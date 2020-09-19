7d2c6ee5f99b67679575a4c3d8f47ebcde8bf3c1
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-20 03:18:21 +0900

    商品情報をDBに保存する機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,22 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :category_id, :condition_id,
+      :ship_method_id, :prefecture_id, :ship_date_id, :price, :image
+    ).merge(user_id: current_user.id)
+  end
 end
