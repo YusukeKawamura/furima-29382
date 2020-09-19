9dbf73e0af59c637fa034aec9aeeca62b5df0756
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-19 21:41:50 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :ship_method
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :ship_date
+  belongs_to :user
 end
