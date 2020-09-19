e83eb5e4013ba55a6f3f736c82843f626376ec2a
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-20 03:34:35 +0900

    バリデーションの再修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,8 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :name, :description, :category, :condition, :ship_method, :prefecture, :ship_date
-    validates :price, numericality: { only_integt: true,
+    validates :image, :name, :description, :category, :condition, :ship_method, :prefecture, :ship_date
+    validates :price, numericality: { only_integer: true,
                                       greater_than_or_equal_to: 300,
                                       less_than_or_equal_to: 9_999_999 }
   end
