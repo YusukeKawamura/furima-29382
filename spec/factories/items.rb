e0cf51447c101e342c15bf1f965fb8144dbd3070
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-22 04:16:12 +0900

    Test用アイテムの生成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name { 'テスト商品' }
+    description { '商品説明が入力されている' }
+    category_id  { 2 }
+    condition_id { 2 }
+    ship_method_id { 2 }
+    prefecture_id { 2 }
+    ship_date_id { 2 }
+    price { 1000 }
+    association :user
+
+    after(:build) do |picture|
+      picture.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
