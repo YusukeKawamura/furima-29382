a91a8e3f3df78e3abea151b14ea137276a69b58d
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-24 16:46:05 +0900

    各モデルのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship_date
   belongs_to :user
   has_one_attached :image
+  has_one :order
 
   with_options presence: true do
     validates :image, :name, :description, :category, :condition, :ship_method, :prefecture, :ship_date
