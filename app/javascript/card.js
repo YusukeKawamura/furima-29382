6b4a20952a369dc89425e38e23a1af8aec27dfea
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-25 17:33:34 +0900

    トークンの生成に成功した場合、トークンを埋め込むinputタグを生成する

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -15,6 +15,12 @@ const pay = () => {
     }
 
     Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id
+        const renderDom = document.getElementById("charge-form")
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`
+        renderDom.insertAdjacentHTML("beforeend", tokenObj)
+      }
     })
   })
 }
