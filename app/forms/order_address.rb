d5413a1779417feca865049baf762f9f14ad177d
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-27 17:05:35 +0900

    バリデーションの設定

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -2,6 +2,13 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postcode, :prefecture_id, :municipality, :street, :apartment, :tel, :token, :user_id, :item_id
 
+  with_options presence: true do
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :municipality, :street
+    validates :tel, format: { with: /\A[0-9]+\z/ }
+  end
+
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(
