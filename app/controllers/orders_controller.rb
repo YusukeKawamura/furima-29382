e590a119e7d539e33a193b0dc890547cf90a9f1a
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-27 17:56:49 +0900

    入力した配送先情報をDBに保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,26 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find_by(id: params[:item_id])
+    @order = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find_by(id: params[:item_id])
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(
+      :postcode, :prefecture_id, :municipality,
+      :street, :apartment, :tel
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
