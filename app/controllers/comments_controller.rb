840601f1b4943818453c04d2b91ed42bfba6b9df
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-09-29 20:07:58 +0900

    broadcastを介してcommentを送信

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,12 +4,13 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(comments_params)
+    @comment = Comment.new(comment_params)
+    ActionCable.server.broadcast 'comment_channel', content: @mcomment if @comment.save
   end
 
   private
 
-  def comments_params
+  def comment_params
     params.require(:item).permit(:content).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
