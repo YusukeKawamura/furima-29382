5ba2c03187cc8b7ba9af0f808ed2aa55c298cb67
YusukeKawamura <yus.kawamura0722@gmail.com>
2020-10-02 11:57:40 +0900

    ActionCableをコントローラーに記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,14 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @item = Item.find_by(id: params[:item_id])
     @comment = Comment.new(comment_params)
-    ActionCable.server.broadcast 'comment_channel', content: @mcomment if @comment.save
+    ActionCable.server.broadcast 'comment_channel', content: @comment if @comment.save
   end
 
   private
 
   def comment_params
-    params.require(:item).permit(:content).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.permit(:content).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
